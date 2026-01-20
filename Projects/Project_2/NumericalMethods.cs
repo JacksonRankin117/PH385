6cc5507387e25058342594965bddcba13c7d735d
JacksonRankin117 <ran23008@byui.edu>
2026-01-20 16:20:57 -0700

    Added comments

--- Projects/Project_2/NumericalMethods.cs
+++ Projects/Project_2/NumericalMethods.cs
@@ -5,10 +5,11 @@ class Methods
     {
         while (pend._times.Last() < t_f)
         {
+            // Find the new angular veolcity
             pend._omegas.Add(pend._omegas.Last() + dt * (pend.GravityAcceleration(pend._thetas.Last())
                                                       +  pend.FrictionAcceleration(pend._omegas.Last())
                                                       +  pend.DrivingAcceleration(pend._times.Last())));
-            
+            // Find the new angular position
             pend._thetas.Add(pend._thetas.Last() + pend._omegas.Add()* dt);
         }
     }
