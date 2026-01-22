8e1a3379bfe91c0e03e9c2f5318450a6d636c4eb
JacksonRankin117 <ran23008@byui.edu>
2026-01-22 09:55:38 -0700

    Finished with data analysis. Work on UI next

--- Projects/Project_1/obj/Debug/net10.0/Project_1.AssemblyInfo.cs
+++ Projects/Project_1/obj/Debug/net10.0/Project_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5ace113fffda03cd69df41a852fd0e616df2083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+804b3ce9f05363049ed5f29ef46989c384152916")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Projects/Project_2/obj/Debug/net10.0/Project_2.AssemblyInfo.cs
+++ Projects/Project_2/obj/Debug/net10.0/Project_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5ace113fffda03cd69df41a852fd0e616df2083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8ccdff6656a3318fe9a0661c0dc498a030a13e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
