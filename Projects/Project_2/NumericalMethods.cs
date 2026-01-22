b0c761d7470828a23c3d6ef9cc07c73a00502ac6
JacksonRankin117 <ran23008@byui.edu>
2026-01-22 00:43:18 -0700

    gverv

--- Projects/Project_2/NumericalMethods.cs
+++ Projects/Project_2/NumericalMethods.cs
@@ -1,11 +1,12 @@
 using System.Linq;
 class Methods
 {
+    // Euler's method. Return the time, 
     public static (double, double, double) EulerCromer(Pendulum pend, double dt, double t_f)
     {
         while (pend._times.Last() < t_f)
         {
-            // Find the new angular veolcity
+            // Find the new angular velocity
             pend._omegas.Add(pend._omegas.Last() + dt * (pend.GravityAcceleration(pend._thetas.Last())
                                                       +  pend.FrictionAcceleration(pend._omegas.Last())
                                                       +  pend.DrivingAcceleration(pend._times.Last())));
